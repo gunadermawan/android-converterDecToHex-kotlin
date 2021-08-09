ddcf16166ae0c355e71373c136dda08c51cb073d
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-08-09 10:24:52 +0700

    menambahkan logic konverter to hexa

--- app/src/main/java/com/gunder/converter_decimaltohex/MainActivity.kt
+++ app/src/main/java/com/gunder/converter_decimaltohex/MainActivity.kt
@@ -2,10 +2,28 @@ package com.gunder.converter_decimaltohex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+//    inisialisasi
+    private var input:EditText? = null
+    private var output: EditText? = null
+    private var submit: Button? = null
+    private var reset: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        inisialisasi variabel
+        input = findViewById(R.id.input)
+        output = findViewById(R.id.output)
+        submit = findViewById(R.id.submit)
+        submit!!.setOnClickListener {
+            val string = input!!.text.toString()
+            val i = string.toInt()
+            val binary = Integer.toHexString(i)
+            output!!.setText(binary)
+        }
     }
 }
